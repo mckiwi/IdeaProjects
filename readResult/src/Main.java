5e35f170dd2c22c109d9cb116911580b7d846fa6
mckiwi <b023040037@gmail.com>
2019-05-08 01:30:55 +0800

    analyse

--- readResult/src/Main.java
+++ readResult/src/Main.java
@@ -4,33 +4,47 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String fileName = "result.csv";
+        String fileName = "result-a-0.001.csv";
         String fileOut = "output.csv";
         int lineNum=1;
         try(FileReader fileReader = new FileReader(fileName)){
-            int ch = fileReader.read();
-            int col = 0,col3 = 0;
-            while(ch != -1){
+            try(FileWriter fileWriter = new FileWriter(fileOut,true)) {
+                int ch = fileReader.read();
+                int col = 0,col3 = 0;
+                while(ch != -1){
 
-                if((char)ch == '\n') {
-                    lineNum++;
-                    col=0;
-                }
-
-                if(lineNum%2==0) {
-                    if((char)ch == ',')
-                        col++;
-                    if(col == 2){
-                        if((char)ch == ','){
-                            System.out.println();
-                        }
-                        else
-                        System.out.print((char) ch);
+                    if((char)ch == '\n') {
+                        lineNum++;
+                        col=0;
                     }
-                    //System.out.print((char) ch);
 
+                    if(lineNum%2==0) {
+                        if((char)ch == ',')
+                            col++;
+                        if(col == 2){
+                            if((char)ch == ','){
+                                System.out.println();
+                                String fileContent = "\n";
+                                fileWriter.write(fileContent);
+                            }
+                            else{
+                                //String fileContent = (char)ch;
+                                fileWriter.write(ch);
+                                System.out.print((char) ch);
+                            }
+
+                        }
+                        //System.out.print((char) ch);
+
+                    }
+                    ch = fileReader.read();
                 }
-                ch = fileReader.read();
+
+
+                    /*String fileContent = "This is a sample text.";
+                    fileWriter.write(fileContent);*/
+            } catch (IOException e) {
+                // exception handling
             }
         } catch(FileNotFoundException e){
             System.out.println("File not found");
@@ -38,12 +52,7 @@ public class Main {
 
         }
 
-        /*try(FileWriter fileWriter = new FileWriter(fileOut,true)) {
-            String fileContent = "This is a sample text.";
-            fileWriter.write(fileContent);
-        } catch (IOException e) {
-            // exception handling
-        }*/
+
         System.out.println("\nHello World!"+lineNum);
     }
 }
