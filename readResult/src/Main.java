e736a5e520491b34add6be22126f27d700ba07c6
mckiwi <b023040037@gmail.com>
2019-05-22 00:13:12 +0800

    readResult UeFixed

--- readResult/src/Main.java
+++ readResult/src/Main.java
@@ -6,22 +6,43 @@ public class Main {
 
         String fileName = "result-opt.csv";
         String fileOut = "output.csv";
-        int lineNum=1;
-        try(FileReader fileReader = new FileReader(fileName)){
-            try(FileWriter fileWriter = new FileWriter(fileOut,true)) {
-                int ch = fileReader.read();
-                int col = 0,col3 = 0;
-                while(ch != -1){
+        for(int i=2;i<=40;i+=2){
+            int lineNum=1,tmp=i;
+            try(FileReader fileReader = new FileReader(fileName)){
+                try(FileWriter fileWriter = new FileWriter(fileOut,true)) {
+                    int ch = fileReader.read();
+                    int col = 0;
+                    while(ch != -1){
 
-                    if((char)ch == '\n') {
-                        lineNum++;
-                        col=0;
-                    }
+                        if((char)ch == '\n') {
+                            lineNum++;
+                            col=0;
+                        }
 
-                    if(lineNum%2==0) {
+                        if(lineNum%2==0) {
+                            if((char)ch == ',')
+                                col++;
+                            if(i==40)tmp=0;
+                            if(col == 0 && lineNum%40 == tmp){   //choose column
+                                if((char)ch == ','){
+                                    System.out.println();
+                                    String fileContent = "\n";
+                                    fileWriter.write(fileContent);
+                                }
+                                else{
+                                    //String fileContent = (char)ch;
+                                    fileWriter.write(ch);
+                                    System.out.print((char) ch);
+                                }
+
+                            }
+                            //System.out.print((char) ch);
+
+                        }
+                    /*if(lineNum%2==0) {
                         if((char)ch == ',')
                             col++;
-                        if(col == 2){
+                        if(col == 0){   //choose column
                             if((char)ch == ','){
                                 System.out.println();
                                 String fileContent = "\n";
@@ -36,23 +57,26 @@ public class Main {
                         }
                         //System.out.print((char) ch);
 
+                    }*/
+                        ch = fileReader.read();
                     }
-                    ch = fileReader.read();
-                }
 
 
                     /*String fileContent = "This is a sample text.";
                     fileWriter.write(fileContent);*/
-            } catch (IOException e) {
-                // exception handling
-            }
-        } catch(FileNotFoundException e){
-            System.out.println("File not found");
-        }catch(IOException e){
+                } catch (IOException e) {
+                    // exception handling
+                }
+            } catch(FileNotFoundException e){
+                System.out.println("File not found");
+            }catch(IOException e){
 
+            }
+            System.out.println("\nHello World!"+lineNum);
         }
 
 
-        System.out.println("\nHello World!"+lineNum);
+
+
     }
 }
