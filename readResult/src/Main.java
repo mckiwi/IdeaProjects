36e7844680821fafb07e56e0edc7e936c49a6439
mckiwi <b023040037@gmail.com>
2019-06-03 06:12:10 +0800

    analyse mobility data

--- readResult/src/Main.java
+++ readResult/src/Main.java
@@ -4,60 +4,40 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String fileName = "result-opt.csv";
+        String fileName = "mob-tk-bue.csv";
         String fileOut = "output.csv";
-        for(int i=2;i<=40;i+=2){
+        int take=30;
+        for(int i=1;i<=take;i++){
             int lineNum=1,tmp=i;
             try(FileReader fileReader = new FileReader(fileName)){
                 try(FileWriter fileWriter = new FileWriter(fileOut,true)) {
+
+
+
                     int ch = fileReader.read();
                     int col = 0;
                     while(ch != -1){
 
-                        if((char)ch == '\n') {
+
+
+                        if((char)ch == '\n') {  //new line
                             lineNum++;
                             col=0;
                         }
 
-                        if(lineNum%2==0) {
-                            if((char)ch == ',')
+
+                            if((char)ch == ',') //next column
                                 col++;
-                            if(i==40)tmp=0;
-                            if(col == 0 && lineNum%40 == tmp){   //choose column
-                                if((char)ch == ','){
-                                    System.out.println();
-                                    String fileContent = "\n";
-                                    fileWriter.write(fileContent);
-                                }
-                                else{
-                                    //String fileContent = (char)ch;
-                                    fileWriter.write(ch);
-                                    System.out.print((char) ch);
-                                }
 
-                            }
-                            //System.out.print((char) ch);
-
-                        }
-                    /*if(lineNum%2==0) {
-                        if((char)ch == ',')
-                            col++;
-                        if(col == 0){   //choose column
-                            if((char)ch == ','){
-                                System.out.println();
-                                String fileContent = "\n";
-                                fileWriter.write(fileContent);
-                            }
-                            else{
-                                //String fileContent = (char)ch;
+                            if(tmp==take)tmp=0;
+                            if(lineNum%take==tmp) {
                                 fileWriter.write(ch);
                                 System.out.print((char) ch);
                             }
+                            //System.out.print((char) ch);
 
-                        }
-                        //System.out.print((char) ch);
 
-                    }*/
+
                         ch = fileReader.read();
                     }
 
